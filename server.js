7805b15b21f2ca11fdbffd7b78b79fddc60abb33
austinald1 <81729807+austinald1@users.noreply.github.com>
2021-07-18 21:24:19 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const fs = require('fs');
+
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// middleware --> a helper functioin that works in between the response and request cycle
+// built in express middleware called static
+
+// parse incoming string or array data
+
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+
+app.use(express.json());
+
+// express isn't designed to host static files
+// helps create virtual path between location of the file based on the directory given and the route itself
+app.use(express.static('public'));
+
+const { notes } = require('./db/db');
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+// this should always come last
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
+});
+
+
+
