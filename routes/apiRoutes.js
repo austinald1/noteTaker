95d14d26ded269169d5ea9441f8d748145a7e4c1
austinald1 <81729807+austinald1@users.noreply.github.com>
2021-07-21 22:01:33 -0700

    refractered api post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,45 @@
-// function validateNote(note) {
-//     if (!note.title || typeof note.title !== 'string') {
-//       console.log('bad title format');
-//       return false;
-//     }
-//     if (!note.text || typeof note.text !== 'string') {
-//       console.log('bad text format');
-//       return false;
-//     }
-//     return true;
-//   }
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+      console.log('bad title format');
+      return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+      console.log('bad text format');
+      return false;
+    }
+    return true;
+  }
   
-//   // add a note to json file and notes array in this function
-//   function createNewNote(body, notesArray) {
-//     const note = body;
-//     notesArray.push(note);
-//     fs.writeFileSync(
-//       path.join(__dirname, './data/notes.json'),
-//       JSON.stringify({ notes: notesArray }, null, 2)
-//     );
-//     return note;
-//   }
+  // add a note to json file and notes array in this function
+  function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './data/notes.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  }
   
-//   function deleteNote(id, notesArray) {
-//     notesArray = notesArray.filter((note) => note.id !== id);
-//     fs.writeFileSync(
-//       path.join(__dirname, './data/notes.json'),
-//       JSON.stringify({ notes: notesArray }, null, 2)
-//     );
-//   }
+  function deleteNote(id, notesArray) {
+    notesArray = notesArray.filter((note) => note.id !== id);
+    fs.writeFileSync(
+      path.join(__dirname, './data/notes.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+  }
   const router = require('express').Router();
-
-  router.get('/api/notes', (req, res) => {
+  const fs = require('fs')
+  const path = require('path')
+  const {v4: uuidv4} = require('uuid')
+  let notes = require('../data/notes')
+  router.get('/notes', (req, res) => {
     console.log(notes);
     let results = notes;
     res.json(results);
   });
   
-  router.post('/api/notes', (req, res) => {
+  router.post('/notes', (req, res) => {
     // this only works if no data is removed, otherwise id's will be thrown off
     req.body.id = uuidv4();
     if (!validateNote(req.body)) {
@@ -47,7 +50,7 @@
     }
   });
   
-  router.delete('/api/notes/:id', (req, res) => {
+  router.delete('/notes/:id', (req, res) => {
     const { id } = req.params;
     const deleted = notes.find((note) => note.id === id);
     if (deleted) {
